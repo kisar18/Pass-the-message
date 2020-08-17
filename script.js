e9f74d9558973c765df4549d4e53699584d252e4
kisar18 <martinpur99@seznam.cz>
2020-08-17 08:44:38 +0200

    functional warning

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 var button = document.getElementsByClassName('submit-btn')[0]
 var cont = document.getElementsByClassName('container')[0]
+var warn = document.getElementsByClassName('warning')[0]
 
 button.onclick = function() {
-    if (document.getElementsByClassName('input-msg')[0].value == "")
-    {
-        cont.innerHTML = `
-            <div class="container">
-                <div class="main-title">A Message You Would Like to Pass</div>
-                <input class="input-msg">
-                <button class="submit-btn" type="button">Submit</button>
-                <div class="warning">Please Enter A Value To Pass</div>
-                <div class="last-msg-title">Last Message Delivered</div>
-                <div class="last-msg-input">HELLO WORLD</div>
-            </div>`
+    var inputField = document.getElementsByClassName('input-msg')[0]
+    var lastMessage = document.getElementsByClassName('last-msg-input')[0]
+
+    if (inputField.value === "" && lastMessage.innerHTML == "HELLO WORLD") {
+        warn.style.display = "block"
+        setTimeout(function() {warn.style.display = "none"},3000)
+    }
+    else if (inputField.value === "" && lastMessage.innerHTML != "HELLO WORLD"){
+        warn.style.display = "block"
+        setTimeout(function() {warn.style.display = "none"},3000)
     }
     else {
-        document.getElementsByClassName('last-msg-input')[0].innerHTML = document.getElementsByClassName('input-msg')[0].value.toUpperCase()
-        document.getElementsByClassName('input-msg')[0].value = ""
+        lastMessage.innerHTML = inputField.value.toUpperCase()
+        inputField.value = ""
     }
 }
