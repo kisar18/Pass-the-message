cee5eb09601b9f78021f76c06368b5d1d75303ea
kisar18 <martinpur99@seznam.cz>
2020-08-18 09:54:42 +0200

    reduced js code

--- script.js
+++ script.js
@@ -6,11 +6,7 @@ button.onclick = function() {
     var inputField = document.getElementsByClassName('input-msg')[0]
     var lastMessage = document.getElementsByClassName('last-msg-input')[0]
 
-    if (inputField.value === "" && lastMessage.innerHTML == "HELLO WORLD") {
-        warn.style.display = "block"
-        setTimeout(function() {warn.style.display = "none"},3000)
-    }
-    else if (inputField.value === "" && lastMessage.innerHTML != "HELLO WORLD"){
+    if (inputField.value === "") {
         warn.style.display = "block"
         setTimeout(function() {warn.style.display = "none"},3000)
     }
